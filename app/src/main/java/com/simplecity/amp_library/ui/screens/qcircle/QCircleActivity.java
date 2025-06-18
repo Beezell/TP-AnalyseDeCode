e8ba517df34339f16ed4e8fd5c354a6cb88dcb06
Beezell <abigael.castillo@live.fr>
2025-06-18 15:16:24 +0200

    jsp

--- app/src/main/java/com/simplecity/amp_library/ui/screens/qcircle/QCircleActivity.java
+++ app/src/main/java/com/simplecity/amp_library/ui/screens/qcircle/QCircleActivity.java
@@ -251,11 +251,6 @@ public class QCircleActivity extends BaseActivity {
         if (pauseBtn == null) {
             return;
         }
-        if (MusicServiceConnectionUtils.serviceBinder != null && mediaManager.isPlaying()) {
-
-        } else {
-
-        }
     }
 
     private void initializeBackButton() {
